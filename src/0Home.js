703e8237f31affa55bfac606ac511bf60e6cb0a4
Johnsensei <john@johnsensei.com>
2021-10-27 12:34:17 -0500

    Removed old comment

--- src/0Home.js
+++ src/0Home.js
@@ -47,7 +47,6 @@ const Home = (props) => {
                       console.log(err)
                   }
               }else {
-                  //Try catch here to see if warning stops on music stop
                   await music.stopAsync()
                   await music.unloadAsync()
               }
